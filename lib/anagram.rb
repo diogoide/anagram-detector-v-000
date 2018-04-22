d435c2baa75b0ed400c360af50506e7e7d77046e
Diogo Ide <diogoide@gmail.com>
2018-04-22 18:16:49 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,14 @@
 class Anagram
-  attr_accessor :anagram
+  attr_accessor :anagram #word = anagram
 
   def initialize(anagram)
     @anagram = anagram
   end
 
-  def match(array)
+  def match(array) #array = word_array
     result = []
-    array.select do |item|
-      result = item if @anagram.split(" ").sort == item.split(" ").sort
+    array.select do |item| == #item = word
+      result = item if item.split("").sort == @anagram.split("").sort ==
       end
     end
   end
