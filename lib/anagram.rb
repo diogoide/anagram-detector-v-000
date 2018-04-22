0298e6087c3d5581a79efbe4ca30264fa6943ffd
Diogo Ide <diogoide@gmail.com>
2018-04-22 17:35:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(anagram)
+    self.find {|i| i.reverse == anagram}
+  end
+
+end
